ee0b73c9bdd978491be1dce88e7b1a92b99c713d
noname1579 <ilyalashenko115@gmail.com>
2025-01-27 16:36:31 +0300

    сменил api, улучшил адаптив для телефонов

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,26 @@ import Post from './Post';
 
 export default function App() {
 
-  const url = 'https://api.coinlore.net/api/tickers/'
+  // const url = 'https://api.coinlore.net/api/tickers/'
+  const url = 'https://api.coinranking.com/v2/coins'
   const [data, setData] = useState([]);
+
+  const Data = [
+    {
+      name: 'Btc',
+      logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png'
+    },
+    {
+      name: 'Eth',
+      logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJ0l8zfIqISUA8VCF4SVVZqVCF1NeKHbtVBA&s'
+    }
+  ]
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(url);
       const result = await response.json();
-      setData(result.data);
+      setData(result.data.coins);
     }
     fetchData();
   }, []);
@@ -25,7 +37,7 @@ export default function App() {
       <div className="app">
 
         {data.map(p => (
-          <Post name={p.symbol} fullname={p.name} percent_change_24h={p.percent_change_24h} price={p.price === 0 ? null : Number(p.price_usd).toFixed(2)} />
+          <Post name={p.symbol} key={p.name} logo={p.iconUrl} fullname={p.name} percent_change_24h={p.change} price={p.price === 0 ? null : Number(p.price).toFixed(2)} />
         ))}
       </div>
     </>
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,9 +1,8 @@
 import './styles/Post.css'
-import photo from './img/Bitcoin.svg.png'
 import arrUp from './img/arrUp.png'
 import arrDown from './img/arrDown.png'
 
-export default function Post({name, fullname, percent_change_24h, price}) {
+export default function Post({name, fullname, percent_change_24h, price, logo}) {
 
 
   return(
@@ -11,11 +10,13 @@ export default function Post({name, fullname, percent_change_24h, price}) {
       <div className="container">
         <div className="container_info">
           <div className="container_info_form">
-            <img src={photo} />
-            <div className="name">{name}</div>
-            <div className="stick"> | </div>
-            <div className="fullname">{fullname}</div>
-            <div className="percent_change_24h">{percent_change_24h}{percent_change_24h > 0 ? <img src={arrUp} /> : <img src={arrDown} />} </div>
+            <img src={logo} />
+            <div className="container_info_form_width">
+              <div className="name">{name}</div>
+              <div className="stick"> | </div>
+              <div className="fullname">{fullname}</div>
+            </div>
+            <div className="percent_change_24h">{Math.abs(percent_change_24h)}{percent_change_24h > 0 ? <img src={arrUp} /> : <img src={arrDown} />} </div>
           </div>
           <div className="price">{price} $</div>
           </div>
